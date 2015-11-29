0130a382cd1569f8ff90bbcda7c6125e5c47f565
Merciert <ynot16@hotmail.fr>
2015-11-29 16:34:34 +0100

    Update graphic interface (text area and scroll bar)

--- src/graphics/Window.java
+++ src/graphics/Window.java
@@ -1,4 +1,7 @@
 package graphics;
+import java.awt.BorderLayout;
+import java.awt.Component;
+import java.awt.event.WindowAdapter;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -13,6 +16,8 @@ public class Window {
 	protected JFrame window;
 	protected JMenuBar menuBar;
 	protected List<JMenu> menus = new ArrayList<JMenu>();
+	protected JTextArea textArea;
+	protected JScrollPane textAreaScrollPane;
 	
 	
 	// utile ? 
@@ -26,10 +31,12 @@ public class Window {
 		
 		JMenu file = new JMenu("file");
 		JMenu tools = new JMenu("Tools");
+		JMenu help = new JMenu("Help");
 		// TODO file a passer en parametre
 		//tools.addActionListener(new PluginFinder());
 		this.menus.add(file);
 		this.menus.add(tools);
+		this.menus.add(help);
 	}
 	
 	protected void addMenusToMenuBar() {
@@ -39,23 +46,32 @@ public class Window {
 		}
 	}
 	
+	protected void createTextAreaAndScroll() {
+		this.textArea = new JTextArea("Some text we want to transform",25,100);
+		this.textAreaScrollPane = new JScrollPane(textArea);
+		this.textAreaScrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+		this.textAreaScrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_ALWAYS);
+	}
+	
 	protected void addToPanel(JPanel content) {
-		content.add(this.menuBar);
-		
+		content.add(this.menuBar, BorderLayout.NORTH);
+		content.add(textAreaScrollPane, BorderLayout.CENTER);
 	}
 	
 	public Window() {
-		this.window = new JFrame("Plugins Project");
-		this.window.setSize(200,200);
+		this.window = new JFrame("Extendable Editor");
+		this.window.setSize(400,200);
 		this.window.setLocation(100, 100);
+		this.window.addWindowListener(new FermeWindowEvent());
 		
 		JPanel content = new JPanel();
+		content.setLayout(new BorderLayout());
 		
 		this.createMenus();
 		this.addMenusToMenuBar();
+		this.createTextAreaAndScroll();
 		this.addToPanel(content);
-		
-			
+					
 		this.window.setContentPane(content);
 		this.window.setVisible(true);
 	}
@@ -67,4 +83,15 @@ public class Window {
 		
 		//commandes a executer ? 
 	}
+	
+	
+	/*
+	 * Internal Classes
+	 */
+	
+	class FermeWindowEvent extends WindowAdapter {
+		public void windowClosing(java.awt.event.WindowEvent e) {
+			System.exit(0);
+		}
+	} 
 }
