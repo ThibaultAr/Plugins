dc3bf2db6d7044c7124ee62e514b8b6c8ce14b49
marie <marie.jones@etudiant.univ-lille1.fr>
2015-12-08 10:27:15 +0100

    plugin Test

--- src/plugins/Plugin.java
+++ src/plugins/Plugin.java
@@ -1,20 +1,21 @@
 package plugins;
 
 /**
- *
+ * Plugin Interface. A plugin is an action caracterized by a label. It transforms the text by applying some effects, 
+ * for instance, turning all the caracters into upper case.
  */
 public interface Plugin {
 
 	/**
-	 * 
-	 * @param s
-	 * @return
+	 * changes the String passed as a parameter with the effect of the plugin
+	 * @param s what needs to be transformed by the plugin
+	 * @return the result of the plugin on the String s  
 	 */
 	public String transform(String s);
 	
 	/**
 	 * 
-	 * @return
+	 * @return the plugin's name
 	 */
 	public String getLabel();
 }
